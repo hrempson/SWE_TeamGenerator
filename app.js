addfbb12944fba10f78bcbb84f3e9d3b5f454968
Heather Empson <hrempson@gmail.com>
2020-06-11 15:52:15 -0700

    working but needs fs

--- app.js
+++ app.js
@@ -3,37 +3,171 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
-​
+const fs = require("fs");​
+
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+const outputPath = path.join(OUTPUT_DIR, "team.html");​
+
 const render = require("./lib/htmlRenderer");
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-const questions = [
-    {
-    type: "input",
-    message: "Please enter employee name: ",
-    name: "name"
+
+const employees = [];
+const employeeQuestions = [{
+        type: "input",
+        message: "Please enter employee name: ",
+        name: "name"
     },
     {
-    type: "input",
-    message: "Please enter employee ID: ",
-    name: "id"
+        type: "input",
+        message: "Please enter employee ID: ",
+        name: "id"
     },
     {
-    type: "input",
-    message: "Please enter employee email: ",
-    name: "email"
+        type: "input",
+        message: "Please enter employee email: ",
+        name: "email"
     },
     {
         type: "input",
         message: "Please enter employee role? ",
         name: "role"
     }
-]
+];
+inquirer.prompt(employeeQuestions).then((response) => {
+    console.log(response);
+
+});
+
+function askForEmployeeRole() {
+
+    console.log( "----------------")
+    console.log("Add a new Engineer");
+    console.log( "----------------");
+
+    inquirer.prompt({
+            message: "What is the employee's role?",
+            name: "role",
+            type: "list",
+            choices: [
+                "Engineer",
+                "Intern"
+            ]
+        })
+        .then((response) => {
+            if (response.role === "Engineer") {
+                askForEngineerInfo()
+            } else if (response.role === "Intern") {
+                askForInternInfo();
+            }
+
+        });
+
+    function askForManagerInfo() {
+
+        console.log( "----------------");
+        console.log("Add a new Manager");
+        console.log( "----------------");
+
+        inquirer
+            .prompt([
+                ...employeeQuestions,
+                {
+                    type: "input",
+                    message: "What is the employees phone number?",
+                    name: "phone"
+                }
+            ])
+            .then(({name, id, email, phone}) => {
+                employees.push(new Manager(name, id, email, phone));
+                
+
+                askForEmployeeRole();
+            })
+
+        console.log("ask for manager")
+    }
+
+    function askForInternInfo() {
+
+        console.log( "----------------");
+        console.log("Add a new Intern");
+        console.log( "----------------");
+
+        inquirer.prompt([
+                ...employeeQuestions,
+                {
+                    type: "input",
+                    message: "What school is the intern attending?",
+                    name: "school"
+                }
+            ])
+            .then(({name, id, email, school}) => {
+                employees.push(new Intern(name, id, email, school));
+                new Intern(response.name, response.id, response.email, response.school);
+
+                askToContinue();
+            })
+    }
+
+    function askForEngineerInfo() {
+
+        console.log( "----------------");
+        console.log("Add a new Engineer");
+        console.log( "----------------");
+
+        inquirer.prompt([
+                ...employeeQuestions,
+                {
+                    type: "input",
+                    message: "What is the employee's GitHub username?",
+                    name: "github"
+                }
+            ])
+            .then(({name, id, email, github}) => {
+                employees.push(new Engineer(name, id, email, github));
+                new Engineer(response.name, response.id, response.email, response.github);
+
+                askToContinue();
+            })
+    }
+
+    function askToContinue() {
+        inquirer
+            .prompt({
+                    message: "Do you want to add another team member?",
+                    name: "addNew",
+                    type: "list",
+                    choices: [
+                        "Yes",
+                        "No"
+                    ],
+                }).then(({addNew}))
+                    if (addNew === "yes") {
+                        askForEmployeeRole()
+                    } else {
+                        console.log("Team Built!");
+                        console.log(employees);
+                    }
+    }
+}
+
+function createHtmlFile() {
+    const html = render(employees);
+    if (! fs.exsistSync(OUTPUT_DIR))
+        fs.mkdirSync(OUTPUT_DIR);
+
+    fs.writeFile(outputPath, html, (err) => {
+        if (err) console.log(err);
+        else console.log("HTML file created");
+    });
+}
+
+askForManagerInfo();
+// askForEmployeeRole();
+// askForInternInfo();
+// askToContinue();
+
+// Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 ​
 // After the user has input all employees desired, call the `render` function (required
